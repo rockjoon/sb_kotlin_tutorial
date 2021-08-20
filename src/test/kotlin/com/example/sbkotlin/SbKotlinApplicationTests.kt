2b626f4be117bf4a10923e00f7c3e0c9b5377315
rockjoon <49752338+rockjoon@users.noreply.github.com>
2021-08-20 23:05:37 +0900

    initial commit

---
+++ src/test/kotlin/com/example/sbkotlin/SbKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sbkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SbKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
